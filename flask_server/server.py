099357583c4d1459b2ff9a4fb9b7b39741982719
juni <juniejere6654@gmail.com>
2024-05-28 19:51:09 -0500

    corrected requirements.txt werkzeug version to 2.2.2 so that it is compatible with flask 2.2.3 and modified server.py by deleting import of unused package jsonify

--- flask_server/server.py
+++ flask_server/server.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, request, jsonify, send_from_directory
+from flask import Flask, request, send_from_directory
 from flask_cors import CORS
 from SkillScraper import get_skills_json
 import os
